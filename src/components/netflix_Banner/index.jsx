78bd24cb173ad645009b91be5cb86a2f69c2e1c9
Stakshi <semwaleishta6@gmail.com>
2025-02-12 18:50:04 +0530

    correct spellling

--- src/components/netflix_Banner/index.jsx
+++ src/components/netflix_Banner/index.jsx
@@ -2,7 +2,7 @@ import "./nBanner.css";
 const NBanner = () => {
   return (
     <section className="banner">
-      <h1 className="banner_title">Unlimites movies, TV shows and more</h1>
+      <h1 className="banner_title">Unlimited movies, TV shows and more</h1>
       <p className="banner_subtitle">Starts at 149$. Cancel at any time.</p>
     </section>
   );
